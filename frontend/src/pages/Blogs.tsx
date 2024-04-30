2f693c664ecb218cd901a9df97f325ebdd951e32
Lakhan lal <lakhan191136@dei.ac.in>
2024-04-30 15:22:02 +0530

    Added Blog page

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,40 @@
+import { BlogCard } from "../components/BlogCard";
+
+export const Blogs = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-xl">
+        <BlogCard
+          authorName={"Lakhan"}
+          title={
+            "T20 World Cup selection: Pant in fray with Pandya for vice-captaincy"
+          }
+          content={
+            "T20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincy"
+          }
+          publishedDate="30th Apr 2024"
+        />
+        <BlogCard
+          authorName={"Lakhan"}
+          title={
+            "T20 World Cup selection: Pant in fray with Pandya for vice-captaincy"
+          }
+          content={
+            "T20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincy"
+          }
+          publishedDate="30th Apr 2024"
+        />
+        <BlogCard
+          authorName={"Lakhan"}
+          title={
+            "T20 World Cup selection: Pant in fray with Pandya for vice-captaincy"
+          }
+          content={
+            "T20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincyT20 World Cup selection: Pant in fray with Pandya for vice-captaincy"
+          }
+          publishedDate="30th Apr 2024"
+        />
+      </div>
+    </div>
+  );
+};
