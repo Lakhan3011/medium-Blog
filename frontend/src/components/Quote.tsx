8132877b2b908ecc03d078e9ac668fb313317f74
Lakhan lal <lakhan191136@dei.ac.in>
2024-04-30 11:03:29 +0530

    init_frontend

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg ">
+          <div className="text-3xl font-bold">
+            "The customer service I received was exceptional. The support team
+            went above and beyond to address my concerns."
+          </div>
+          <div className="max-w-xs  text-xl font-semibold mt-4 ">
+            Julius Winnfield
+          </div>
+          <div className="max-w-xs  text-slate-400 text-sm font-light">
+            CEO, Acme Inc
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
