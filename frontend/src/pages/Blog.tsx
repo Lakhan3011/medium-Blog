8af99e9cad9415a59607b2628ca80bc5458022a7
Lakhan lal <lakhan191136@dei.ac.in>
2024-05-01 23:40:33 +0530

    added skeleton

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,4 +1,6 @@
+import { Appbar } from "../components/Appbar";
 import { FullBLog } from "../components/FullBlog";
+import { Spinner } from "../components/Spinner";
 import { useBlog } from "../hooks";
 import { useParams } from "react-router-dom";
 
@@ -7,8 +9,17 @@ export const Blog = () => {
   const { loading, blog } = useBlog({
     id: id || "",
   });
-  if (loading) {
-    return <div>loading...</div>;
+  if (loading || !blog) {
+    return (
+      <div>
+        <Appbar />
+        <div className="h-screen flex flex-col justify-center">
+          <div className="flex justify-center">
+            <Spinner />
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <div>
